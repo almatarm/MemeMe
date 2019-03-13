f8dcb435de5c7e60438229d9f666d9e0a9677b1a
Mufeed AlMatar <almatarm@Gala.local>
2019-03-13 13:30:57 +0300

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Mufeed AlMatar on 2019-03-13.
+//  Copyright © 2019 Mufeed AlMatar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,6 @@
+
+# MemeMe #
+
+An swift app that enables a user to take a picture, and add text at the top and bottom to form a meme. The user will be able to share the photo on Facebook and Twitter and also by SMS or email.
+
+This is a requirment for iOS nanodregree from Udacity
