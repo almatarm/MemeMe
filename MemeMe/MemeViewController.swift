a99a93246a212d30fb887e338d4b00029f243941
Mufeed AlMatar <almatarm@Gala.local>
2019-03-14 13:31:44 +0300

    Keyboard Adjustments

--- MemeMe/MemeViewController.swift
+++ MemeMe/MemeViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    var keyboardHeight : CGFloat = 226
+    
     enum ImageSourceType: Int {
         case camera = 0, album
     }
@@ -32,6 +33,16 @@ class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         //Do we have a camera
         pickCameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
         configureUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        addKeyboardObservers()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+//        unsubscribeFromKeyboardNotifications()
     }
 
     // MARK: Configure GUI
@@ -92,5 +103,72 @@ class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         //User cancel image picked
         dismiss(animated: true, completion: nil)
     }
+    
+    // MARK: Keyboard Hide/Show
+    func addKeyboardObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+        self.keyboardHeight = self.getKeyboardHeight(notification)
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if keyboardSize.height != 0 && self.keyboardHeight > keyboardSize.height {
+                keyboardHeight = keyboardSize.height
+            }
+            if isKeyboardOverUI(notification, self.getSelectedTextField() ?? UITextField()) {
+                moveFrameUp()
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        moveFrameBack()
+    }
+    
+    func moveFrameUp() {
+        if self.view.frame.origin.y != self.view.frame.origin.y - keyboardHeight {
+            self.view.frame.origin.y =  -keyboardHeight
+        }
+    }
+    
+    func moveFrameBack() {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func isKeyboardOverUI(_ notification: Notification, _ view: UITextField) -> Bool {
+        keyboardHeight = getKeyboardHeight(notification)
+        return view.frame.maxY > self.view.frame.height - keyboardHeight + 32
+    }
+    
+    // https://stackoverflow.com/questions/30918732/how-to-determine-which-textfield-is-active-swift
+    func getSelectedTextField() -> UITextField? {
+        let totalTextFields = self.getTextFieldsInView(view: self.view)
+        for textField in totalTextFields{
+            if textField.isFirstResponder{
+                return textField
+            }
+        }
+        return nil
+    }
+    
+    func getTextFieldsInView(view: UIView) -> [UITextField] {
+        var totalTextFields = [UITextField]()
+        for subview in view.subviews as [UIView] {
+            if let textField = subview as? UITextField {
+                totalTextFields += [textField]
+            } else {
+                totalTextFields += getTextFieldsInView(view: subview)
+            }
+        }
+        return totalTextFields
+    }
 }
-
