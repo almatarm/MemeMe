c8cd794e62e23ae7f1112b557426d3798f343816
Mufeed H AlMatar <almatarm@gmail.com>
2019-03-16 15:33:09 +0300

    Basic collection view implmentation

---
+++ MemeMe/MemeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Mufeed AlMatar on 16/03/2019.
+//  Copyright © 2019 Mufeed AlMatar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var memeImage: UIImageView!
+}
--- MemeMe/MemeCollectionViewController.swift
+++ MemeMe/MemeCollectionViewController.swift
@@ -9,9 +9,53 @@
 import UIKit
 
 class MemeCollectionViewController: UICollectionViewController {
+    
+    static let CellIdentifier = "MemeCollectionCellIdentifier"
     var memes: [Meme]! {
         return (UIApplication.shared.delegate as! AppDelegate).memes
     }
     
     @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
+    override func viewDidLoad() {
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showMemeEditor))
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        collectionView.reloadData()
+    }
+    
+    @objc func showMemeEditor() {
+        let memeEditor = self.storyboard!.instantiateViewController(withIdentifier: "MemeEditorViewController") as! MemeViewController
+        self.present(memeEditor, animated: true, completion: nil)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return memes.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier:
+            MemeCollectionViewController.CellIdentifier, for: indexPath) as! MemeCollectionViewCell
+        let meme = self.memes[(indexPath as NSIndexPath).row]
+        cell.memeImage.image = meme.memedImage
+        return cell
+    }
+    
+    //    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: MemeTableViewController.CellIdentifier)
+//        let meme = self.memes[(indexPath as NSIndexPath).row]
+//
+//        cell?.textLabel?.text = meme.topText + "..." + meme.bottomText
+//        cell?.imageView?.image = meme.memedImage
+//
+//        return cell!
+//    }
+//
+//    //Define the height of the cell
+//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 72
+//    }
+//
 }
